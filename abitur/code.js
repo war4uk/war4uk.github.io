973f17eb8e00facf6b150d714c671639dd876918
Alex Polyakov <war4uk@users.noreply.github.com>
2019-07-10 23:05:56 +0300

    add spbu bio and med

--- abitur/code.js
+++ abitur/code.js
@@ -10,18 +10,26 @@ async function parser() {
     var firstMedSportivnoeEntries = await fetch("./1med.sportivnoe.txt")
         .then(res => res.text()).then(text => text.split("\r\n").map(parse1medEntry).map(entry => ({...entry, appliedTo1medSportivnoe: true})));
 
+    var spbuLechebnoe = await fetch("./spbu.lechebnoe.txt").then(res => res.text())
+        .then(text => text.split("\r\n").map(parseSpbuMedEntry).map(entry => ({...entry, appliedTospbuMed: true})));
+
+    var spbuBio = await fetch("./spbu.bio.txt").then(res => res.text())
+        .then(text => text.split("\r\n").map(parseSpbuBioEntry).map(entry => ({...entry, appliedToSpbuBio: true})));        
+
     var models = {};
 
     almazovEntries
         .concat(firstMedLechebnoeEntries)
         .concat(firstMedSportivnoeEntries)
+        .concat(spbuLechebnoe)       
+        .concat(spbuBio)                 
         .filter(entry => !!entry)
         .forEach(entry => {
            var model = models[entry.name] || {};
            models[entry.name] = {...model, ...entry};
         });
 
-    var participantArray =     Object.values(models).filter(entry => !!entry.name);
+    var participantArray = Object.values(models).filter(entry => !!entry.name);
     participantArray.sort((entryA, entryB) => {return (+entryB.result) - (+entryA.result);})
         
     var app4 = new Vue({
@@ -30,6 +38,8 @@ async function parser() {
             ALmazovEntries: participantArray.filter(entry => entry.appliedToAlmazov), 
             firstMedLechebnoe: participantArray.filter(entry => entry.appliedTo1medLechebnoe),
             firstMedSportivnoe: participantArray.filter(entry => entry.appliedTo1medSportivnoe),
+            spbuMed: participantArray.filter(entry => entry.appliedTospbuMed), 
+            spbuBio: participantArray.filter(entry => entry.appliedToSpbuBio),                          
         }
       })
 
@@ -38,11 +48,31 @@ async function parser() {
 }
 
 
-function renderAlmazov(participantArray) 
-{
-    var container = document.getElementById("almazov");
-    container.append(new HTMLElement(""))
+function parseSpbuMedEntry(text) {
+    var cells = text.split("	").map(cell => cell.trim());
+    if (cells[4] != "общ.") {
+        return {};
+    }
+    return {
+        name: cells[2],
+        result: (cells[6] || "").split(",")[0],
+        spbuMedOriginal: cells[12] == "Да",
+        spbuMedNumber: cells[0]
+    }
+}
 
+function parseSpbuBioEntry(text) {
+    var cells = text.split("	").map(cell => cell.trim());
+
+    if (cells[4] != "общ.") {
+        return {};
+    }
+    return {
+        name: cells[2],
+        result: (cells[6] || "").split(",")[0],
+        spbuBioOriginal: cells[12] == "Да",
+        spbuBioNumber: cells[0]
+    }
 }
 
 function parse1medEntry(text) {
